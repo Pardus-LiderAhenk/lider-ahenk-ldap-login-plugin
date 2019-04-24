d7544ad533c3de4c5ddf8fba2dcba3b52fc7fef7
hasankara <h.kara27@gmail.com>
2019-04-24 15:16:44 +0300

    LDAP login process will be made with SSSD package

--- lider-console-ldap-login/src/tr/org/liderahenk/ldaplogin/dialogs/LDAPLoginTaskDialog.java
+++ lider-console-ldap-login/src/tr/org/liderahenk/ldaplogin/dialogs/LDAPLoginTaskDialog.java
@@ -44,7 +44,7 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 	
 	private Text textLDAPServerIP;
 	private Text textLDAPDN;
-	private Text textLDAPVersion;
+	//private Text textLDAPVersion;
 	private Text textLDAPAdminDN;
 	private Text textLDAPAdminPassword;
 	private Button buttonCancelLDAPLogin;	
@@ -125,13 +125,13 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 		textLDAPAdminPassword.setEnabled(false);
 		
 		//LDAP Version
-		lblLDAPVersion = new Label(composite, SWT.NONE);
-		lblLDAPVersion.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
-		lblLDAPVersion.setText(Messages.getString("LDAP_VERSION")); //$NON-NLS-1$
-
-		textLDAPVersion=new Text(composite, SWT.BORDER);
-		textLDAPVersion.setLayoutData(new GridData(SWT.FILL,SWT.CENTER,true,false,1,1));
-		textLDAPVersion.setText("3");
+//		lblLDAPVersion = new Label(composite, SWT.NONE);
+//		lblLDAPVersion.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+//		lblLDAPVersion.setText(Messages.getString("LDAP_VERSION")); //$NON-NLS-1$
+//
+//		textLDAPVersion=new Text(composite, SWT.BORDER);
+//		textLDAPVersion.setLayoutData(new GridData(SWT.FILL,SWT.CENTER,true,false,1,1));
+//		textLDAPVersion.setText("3");
 		
 		buttonCancelLDAPLogin.addSelectionListener(new SelectionListener() {
 			
@@ -149,7 +149,7 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 					lblLDAPAdminPassword.setVisible(false);
 					textLDAPAdminPassword.setVisible(false);
 					lblLDAPVersion.setVisible(false);
-					textLDAPVersion.setVisible(false);
+					//textLDAPVersion.setVisible(false);
 				}
 				else {
 					isCancelLDAPLoginChecked = false;
@@ -162,7 +162,7 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 					lblLDAPAdminPassword.setVisible(true);
 					textLDAPAdminPassword.setVisible(true);
 					lblLDAPVersion.setVisible(true);
-					textLDAPVersion.setVisible(true);
+					//textLDAPVersion.setVisible(true);
 				}
 			}
 			
@@ -176,18 +176,18 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 
 	@Override
 	public void validateBeforeExecution() throws ValidationException {
-		if(!textLDAPVersion.getText().equals("")) {
-			try {
-				Integer.parseInt(textLDAPVersion.getText());
-			} catch (Exception e) {
-				MessageDialog.openWarning(shell, Messages.getString("LDAP_VERSION"), Messages.getString("LDAP_VERSION_NOT_INTEGER_ERROR"));
-				throw new ValidationException(Messages.getString("LDAP_VERSION_NOT_INTEGER_ERROR"));
-			}
-		}
-		else {
-			MessageDialog.openWarning(shell, Messages.getString("LDAP_VERSION"), Messages.getString("LDAP_VERSION_EMPTY_ERROR"));
-			throw new ValidationException(Messages.getString("LDAP_VERSION_EMPTY_ERROR"));
-		}
+//		if(!textLDAPVersion.getText().equals("")) {
+//			try {
+//				Integer.parseInt(textLDAPVersion.getText());
+//			} catch (Exception e) {
+//				MessageDialog.openWarning(shell, Messages.getString("LDAP_VERSION"), Messages.getString("LDAP_VERSION_NOT_INTEGER_ERROR"));
+//				throw new ValidationException(Messages.getString("LDAP_VERSION_NOT_INTEGER_ERROR"));
+//			}
+//		}
+//		else {
+//			MessageDialog.openWarning(shell, Messages.getString("LDAP_VERSION"), Messages.getString("LDAP_VERSION_EMPTY_ERROR"));
+//			throw new ValidationException(Messages.getString("LDAP_VERSION_EMPTY_ERROR"));
+//		}
 	}
 	
 	@Override
@@ -201,7 +201,7 @@ public class LDAPLoginTaskDialog extends DefaultTaskDialog {
 			params.put("dn", LdapConnectionListener.getConnection().getConnectionParameter().getExtendedProperty("ldapbrowser.baseDn"));
 			params.put("admin-dn", UserSettings.USER_DN);
 			params.put("admin-password", UserSettings.USER_PASSWORD);
-			params.put("version", textLDAPVersion.getText());
+			//params.put("version", textLDAPVersion.getText());
 		}
 		return params;
 	}
